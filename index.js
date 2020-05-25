8891bc9e58f32328373af202199898a680edaae2
ACAPP-dev <acapp909@gmail.com>
2020-05-25 16:23:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your Circle class here
+
+class Circle {
+
+    constructor(radius) {
+        this.radius = radius
+    }
+
+    get diameter() {
+        return this.radius * 2
+    }
+
+    get circumference() {
+        return this.diameter * Math.PI
+    }
+
+    get area() {
+        return (this.radius * this.radius) * Math.PI
+    }
+
+    set diameter(newDiameter) {
+        this.radius = newDiameter / 2
+    }
+
+    set circumference(newCirc) {
+        this.radius = (newCirc / Math.PI) / 2
+    }
+
+    set area(newArea) {
+        this.radius = Math.sqrt((newArea / Math.PI))
+    }
+}
